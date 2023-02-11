ec1917e279b24f78e956c74c76e672a368ce084b
Alberto Marin <alberto.marind@gmail.com>
2023-02-11 00:27:22 -0600

    Calculadora v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,72 @@
-// let nombre = prompt('Dame tu nombre');
-// alert(nombre);
-// let button = document.getElementById('myButton');
+const PLUS = '+';
+const MINUS = '-';
+const MULTIPLY = '*';
+const DIVIDE = '/';
+const CLEAR = 'C';
+const EQUAL = '=';
 
-
-// let buttonByQuerySelector = document.querySelector('button')
-// buttonByQuerySelector.style.backgroundColor = 'green';
-// buttonByQuerySelector.style.color = 'white';
-
-// let buttonClicked;
-
-// function onClickButton(event) {
-//     let textClicked = event.target.textContent;
-//     let display = document.getElementById('display');
-//     display.textContent = textClicked;
-//     buttonClicked = textClicked;
-// }
-
-let number;
-
-let buttons = document.querySelectorAll('.calculadora .button');
-buttons.forEach((button) => {
-
-    switch (button.textContent) {
-        case '+':
-            button.addEventListener('click', function (event) {
-               number = document.getElementById('display').textContent; 
-               document.getElementById('display').textContent = '';
-            });
-            break;
-        case '-':
-            button.addEventListener('click', function (event) {
-
-            });
-            break;
-        case '*':
-            button.addEventListener('click', function (event) {
-
-            });
-            break;
-        case '/':
-            button.addEventListener('click', function (event) {
-
-            });
-            break;
+let display = {
+    ref: document.getElementById('display'),
+    draw: function (newVal) {
+        this.ref.textContent = newVal;
+    },
+    clear: function () {
+        this.ref.textContent = '';
     }
+};
 
-    button.addEventListener('click', function (event) {
-        let display = document.getElementById('display');
-        display.textContent = display.textContent + event.target.textContent;
-    })
-})
+let calculator = {
+    currentQuantity: '',
+    history: [],
+    handleClear: function () {
+        display.clear();
+    },
+    handleEquals: function () {
+        this.history.push(this.currentQuantity);
+        let quantityToEval = this.history.toString().replaceAll(',', '');
+        let result = Function('return ' + quantityToEval)();
+        display.draw(result);
+        this.history = [];
+        this.history.push(String(result));
+    },
+    handleOperations: function (event) {
+        let symbol = event.target.textContent
+        this.history.push(this.currentQuantity);
+        switch (symbol) {
+            case PLUS:
+                this.history.push(PLUS);
+                break;
+            case MINUS:
+                this.history.push(MINUS);
+                break;
+            case MULTIPLY:
+                this.history.push(MULTIPLY);
+                break;
+            case DIVIDE:
+                this.history.push(DIVIDE);
+                break;
+        }
+        display.clear();
+        this.currentQuantity = '';
+    },
+    handleNumbers: function (event) {
+        this.currentQuantity = this.currentQuantity + event.target.textContent;
+        display.draw(this.currentQuantity);
+    }
+}
 
-
-
+document.querySelectorAll('.calculadora .button').forEach(function (button) {
+    let symbol = button.textContent;
+    if (symbol == PLUS ||
+        symbol == MINUS ||
+        symbol == MULTIPLY ||
+        symbol == DIVIDE) {
+        button.addEventListener('click', calculator.handleOperations.bind(calculator));
+    } else if (symbol == EQUAL) {
+        button.addEventListener('click', calculator.handleEquals.bind(calculator));
+    } else if (symbol == CLEAR) {
+        button.addEventListener('click', calculator.handleClear);
+    } else {
+        button.addEventListener('click', calculator.handleNumbers.bind(calculator));
+    }
+});
